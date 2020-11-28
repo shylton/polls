dffac2ca900239d9ddc28339a35ca73b07551a49
shylton <mattas@oregonstate.edu>
2020-11-28 10:15:07 -0300

    added midware classes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -52,6 +52,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 ROOT_URLCONF = 'djangoProject.urls'
 
 TEMPLATES = [
